95ba18d24fd37600471d996333a5cde14eecbecc
ldmotta <ldmotta@visie.com.br>
2011-07-04 18:06:37 -0300

    Passando o id da aplicação

--- catalog/model/payment/cobredireto.php
+++ catalog/model/payment/cobredireto.php
@@ -19,7 +19,7 @@ class ModelPaymentCobredireto extends Model
     {
 		$this->load->language('payment/cobredireto');
         $method_data = array( 
-                'code'       => 'cobredireto',
+                'id'         => 'cobredireto',
                 'title'      => $this->language->get('text_title'),
                 'sort_order' => $this->config->get('cobredireto_sort_order')
                 );
